4385d580d0d79a545ff3ea1d2d4cd3471bd1f873
keefellow <keefellow@users.noreply.github.com>
2022-07-31 20:30:20 +0800

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -4,13 +4,14 @@ library(randomForestSRC)
 rf_model <-readRDS("model.Rds")
 
 #* @filter cors
-  res$setHeader("Access-Control-Allow-Origin","*")
+cors<-function(req,res){
+    res$setHeader("Access-Control-Allow-Origin","*")
   if (req$REQUEST_METHOD == "OPTIONS"){
         res$setHeader("Access-Control-Allow-Methods","*")
         res$setHeader("Access-Control-Allow-Headers",req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
         res$status <-200
         return(list())
-        else {
+        }else {
         plumber::forward()  
         }
 }
@@ -30,4 +31,4 @@ function(req,res){
   
   }
   
-} 
+ 
